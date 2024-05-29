de8b62debd7e8933446daa07a4a972bbe002acf0
k.sitnikova <k.sitnikova@ipex.global>
2024-05-29 21:23:42 +0300

    fix linter errors

--- src/EvenCli.php
+++ src/EvenCli.php
@@ -5,7 +5,6 @@ namespace BrainGames\Src\EvenCli;
 use function cli\line;
 use function cli\prompt;
 
-
 function startEvenGame()
 {
     $rounds = 3;
@@ -17,20 +16,19 @@ function startEvenGame()
         $randomNum = rand(1, 50);
         $isEven = $randomNum % 2 === 0;
         $question = prompt("Question: ${randomNum}");
-        if($question === 'yes') {
-            if($isEven){
+        if ($question === 'yes') {
+            if ($isEven) {
                 line('Correct!');
             } else {
                 line("'yes' is wrong answer ;(. Correct answer was 'no'");
                 line("Let's try again, %s!", $name);
                 break;
             }
-        } elseif($question === 'no') {
-            if($isEven){
+        } elseif ($question === 'no') {
+            if ($isEven) {
                 line("'yes' is wrong answer ;(. Correct answer was 'no'");
                 line("Let's try again, %s!", $name);
                 break;
-
             } else {
                 line('Correct!');
             }
@@ -39,7 +37,7 @@ function startEvenGame()
         }
         $rounds--;
     }
-    if($rounds === 0) {
+    if ($rounds === 0) {
         line("Congratulations, %s!", $name);
     }
 }
