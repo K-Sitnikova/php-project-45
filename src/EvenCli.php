50141ef7f7af2d9fa5e6a462f2f888c6b55bcc04
k.sitnikova <k.sitnikova@ipex.global>
2024-05-29 21:22:04 +0300

    first change logic

--- src/EvenCli.php
+++ src/EvenCli.php
@@ -5,32 +5,41 @@ namespace BrainGames\Src\EvenCli;
 use function cli\line;
 use function cli\prompt;
 
+
 function startEvenGame()
 {
+    $rounds = 3;
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line('Answer "yes" if the number is even, otherwise answer "no".');
-    $first = prompt('Question: 15');
-    if ($first == 'no') {
-        line('Correct!');
-        $second = prompt('Question: 8');
-        if ($second == 'yes') {
-            line('Correct!');
-            $third = prompt('Question: 2');
-            if ($third == 'yes') {
+    while ($rounds >= 1) {
+        $randomNum = rand(1, 50);
+        $isEven = $randomNum % 2 === 0;
+        $question = prompt("Question: ${randomNum}");
+        if($question === 'yes') {
+            if($isEven){
                 line('Correct!');
-                line("Congratulations, %s!", $name);
             } else {
-                line("'no' is wrong answer ;(. Correct answer was 'yes'");
+                line("'yes' is wrong answer ;(. Correct answer was 'no'");
                 line("Let's try again, %s!", $name);
+                break;
+            }
+        } elseif($question === 'no') {
+            if($isEven){
+                line("'yes' is wrong answer ;(. Correct answer was 'no'");
+                line("Let's try again, %s!", $name);
+                break;
+
+            } else {
+                line('Correct!');
             }
         } else {
-            line("'no' is wrong answer ;(. Correct answer was 'yes'");
             line("Let's try again, %s!", $name);
         }
-    } else {
-        line("'yes' is wrong answer ;(. Correct answer was 'no'");
-        line("Let's try again, %s!", $name);
+        $rounds--;
+    }
+    if($rounds === 0) {
+        line("Congratulations, %s!", $name);
     }
 }
